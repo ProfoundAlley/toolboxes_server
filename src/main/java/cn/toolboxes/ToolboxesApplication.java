e58506cfbe7122fc0d1d1e2a02ea2b1339b1ad8e
profoundalley <profoundalley97@gmail.com>
2023-11-25 22:55:59 +0800

    工具箱 Server 端

---
+++ src/main/java/cn/toolboxes/ToolboxesApplication.java
@@ -0,0 +1,13 @@
+package cn.toolboxes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolboxesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolboxesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/toolboxes/ToolboxesApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.toolboxes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolboxesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
